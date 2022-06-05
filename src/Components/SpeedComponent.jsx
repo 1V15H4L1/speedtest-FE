84fa677d310cb0b10d5da768cc656e45205a89af
Vishal <p.vishal140220@gmail.com>
2022-06-05 18:07:45 +0530

    sessionstorage bug fix

--- src/Components/SpeedComponent.jsx
+++ src/Components/SpeedComponent.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../Styles/DisplayComponent.css";
 function SpeedComponent(props) {
-	let name = sessionStorage.getItem("Name").toUpperCase()? sessionStorage.getItem("Name").toUpperCase() : "user";
+	let name = sessionStorage.getItem("Name")
+		? "user"
+		: sessionStorage.getItem("Name").toUpperCase();
 	return (
 		<>
-			{props.Message === "Matched" && props.Speed>45 && (<div className="speed">Soo fast....!</div>)}
-			{props.Message === "Matched" && (<div className="speed">Hello {name} your speed is {props.Speed} words per minute.</div>)}
+			{props.Message === "Matched" && props.Speed > 45 && (
+				<div className="speed">Soo fast....!</div>
+			)}
+			{props.Message === "Matched" && (
+				<div className="speed">
+					Hello {name} your speed is {props.Speed} words per minute.
+				</div>
+			)}
 		</>
 	);
 }
